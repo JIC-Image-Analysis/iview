9a0e506f1b93b4c3944c776f67f0ebdae1313561
Matthew Hartley <mhartley@cantab.net>
2018-07-17 22:19:26 +0100

    Fix camera and add toggle

--- scripts/simple.py
+++ scripts/simple.py
@@ -16,11 +16,20 @@ view = canvas.central_widget.add_view()
 # img_data = np.random.normal(size=(100, 100, 3), loc=128,
 #                             scale=50).astype(np.ubyte)
 img_data = imread('data/sample0.jpg')
-
+image2 = scene.visuals.Image(imread('data/padded.png'), parent=view.scene)
 image = scene.visuals.Image(img_data, parent=view.scene)
+
+image2.visible = False
 
 # Set 2D camera (the camera will scale to the contents in the scene)
 view.camera = scene.PanZoomCamera(aspect=1)
+view.camera.set_range()
+
+@canvas.events.key_press.connect
+def key_event(event):
+	image.visible = not image.visible
+	image2.visible = not image2.visible
+
 
 if __name__ == '__main__' and sys.flags.interactive == 0:
     app.run()
