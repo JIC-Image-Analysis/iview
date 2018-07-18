4ee8ab0ec4e7f428977eade8188b647f1221cfdd
Matthew Hartley <mhartley@cantab.net>
2018-07-18 16:40:00 +0100

    More experiments

--- scripts/single.py
+++ scripts/single.py
@@ -6,6 +6,15 @@ import numpy as np
 from imageio import imread
 
 
+colors = {
+    1: [255, 0, 0],
+    2: [0, 255, 0],
+    3: [0, 0, 255],
+    4: [255, 255, 0],
+    5: [255, 0, 255],
+    6: [0, 255, 255]
+}
+
 canvas = scene.SceneCanvas(keys='interactive')
 canvas.size = 800, 600
 canvas.show()
@@ -16,20 +25,19 @@ view = canvas.central_widget.add_view()
 # Create the image
 # img_data = np.random.normal(size=(100, 100, 3), loc=128,
 #                             scale=50).astype(np.ubyte)
-img_data = imread('data/T15.png')
-image2 = scene.visuals.Image(imread('data/T15-membrane-cropped.png'), parent=view.scene)
-image = scene.visuals.Image(img_data, parent=view.scene)
+img_data = imread('data/fixed_mask_1.png')
+xdim, ydim = img_data.shape
 
-image2.visible = False
+im = np.zeros((xdim, ydim, 3), dtype=np.uint8)
+for k, v in colors.items():
+    im[np.where(img_data == k)] = v
+image = scene.visuals.Image(im, parent=view.scene)
 
 # Set 2D camera (the camera will scale to the contents in the scene)
 view.camera = scene.PanZoomCamera(aspect=1)
 view.camera.set_range()
+view.camera.flip = (False, True, False)
 
-@canvas.events.key_press.connect
-def key_event(event):
-	image.visible = not image.visible
-	image2.visible = not image2.visible
 
 
 if __name__ == '__main__' and sys.flags.interactive == 0:
--- scripts/tagger.py
+++ scripts/tagger.py
@@ -1,9 +1,12 @@
+from pathlib import Path
+
 import sys
 from vispy import scene
 from vispy import app
 import numpy as np
 
 from imageio import imread
+
 
 
 canvas = scene.SceneCanvas(keys='interactive')
@@ -16,21 +19,37 @@ view = canvas.central_widget.add_view()
 # Create the image
 # img_data = np.random.normal(size=(100, 100, 3), loc=128,
 #                             scale=50).astype(np.ubyte)
-img_data = imread('data/T15.png')
-image2 = scene.visuals.Image(imread('data/T15-membrane-cropped.png'), parent=view.scene)
-image = scene.visuals.Image(img_data, parent=view.scene)
-
-image2.visible = False
+tags = {}
+imdir = Path(sys.argv[1])
+fn_iter = imdir.iterdir()
+fn = next(fn_iter)
+im = imread(fn)
+image = scene.visuals.Image(im, parent=view.scene)
 
 # Set 2D camera (the camera will scale to the contents in the scene)
 view.camera = scene.PanZoomCamera(aspect=1)
 view.camera.set_range()
+view.camera.flip = (False, True, False)
+
+key_to_tag = {
+    'Q': 'dormant',
+    'P': 'emerged'
+}
 
 @canvas.events.key_press.connect
 def key_event(event):
-	image.visible = not image.visible
-	image2.visible = not image2.visible
+    global fn
+    tags[fn] = key_to_tag[event.key.name]
+    try:
+        fn = next(fn_iter)
+    except StopIteration:
+        with open('tags.txt', 'w') as fh:
+            for fn, tag in tags.items():
+                fh.write('{},{}\n'.format(fn, tag))
+        app.quit()
 
+    image.set_data(imread(fn))
+    canvas.update()
 
 if __name__ == '__main__' and sys.flags.interactive == 0:
     app.run()
