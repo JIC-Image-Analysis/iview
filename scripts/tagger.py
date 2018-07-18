72bb72e9df5152ea23ee6071b13f0d6508732bc6
Matthew Hartley <mhartley@cantab.net>
2018-07-18 22:46:32 +0100

    Make tagger use dataset

--- scripts/tagger.py
+++ scripts/tagger.py
@@ -7,29 +7,16 @@ import numpy as np
 
 from imageio import imread
 
+import click
+
+from dtoolcore import DataSet
 
 
 canvas = scene.SceneCanvas(keys='interactive')
 canvas.size = 800, 600
 canvas.show()
 
-# Set up a viewbox to display the image with interactive pan/zoom
 view = canvas.central_widget.add_view()
-
-# Create the image
-# img_data = np.random.normal(size=(100, 100, 3), loc=128,
-#                             scale=50).astype(np.ubyte)
-tags = {}
-imdir = Path(sys.argv[1])
-fn_iter = imdir.iterdir()
-fn = next(fn_iter)
-im = imread(fn)
-image = scene.visuals.Image(im, parent=view.scene)
-
-# Set 2D camera (the camera will scale to the contents in the scene)
-view.camera = scene.PanZoomCamera(aspect=1)
-view.camera.set_range()
-view.camera.flip = (False, True, False)
 
 key_to_tag = {
     'Q': 'dormant',
@@ -38,18 +25,43 @@ key_to_tag = {
 
 @canvas.events.key_press.connect
 def key_event(event):
-    global fn
-    tags[fn] = key_to_tag[event.key.name]
+
+    app.tags[app.current_id] = key_to_tag[event.key.name]
+
     try:
-        fn = next(fn_iter)
+        im, app.current_id = next(app.image_generator)
+        app.image.set_data(im)
+        canvas.update()
     except StopIteration:
-        with open('tags.txt', 'w') as fh:
-            for fn, tag in tags.items():
-                fh.write('{},{}\n'.format(fn, tag))
+        app.dataset.put_overlay("classes", app.tags)
         app.quit()
 
-    image.set_data(imread(fn))
-    canvas.update()
 
-if __name__ == '__main__' and sys.flags.interactive == 0:
+def dataset_image_generator(dataset):
+
+    for i in dataset.identifiers:
+        yield imread(dataset.item_content_abspath(i)), i
+
+
+@click.command()
+@click.argument("dataset_uri")
+def main(dataset_uri):
+
+    dataset = DataSet.from_uri(dataset_uri)
+    app.dataset = dataset
+    app.tags = {}
+
+    app.image_generator = dataset_image_generator(dataset)
+
+    im, app.current_id = next(app.image_generator)
+    app.image = scene.visuals.Image(im, parent=view.scene)
+
+    view.camera = scene.PanZoomCamera(aspect=1)
+    view.camera.set_range()
+    view.camera.flip = (False, True, False)
+
     app.run()
+
+
+if __name__ == '__main__':
+    main()
